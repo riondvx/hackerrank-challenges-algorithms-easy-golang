6fc49523973f875c6041828d64b925363157658a
Rion <rioneffendy0001repo@gmail.com>
2024-08-09 20:06:05 +0700

    Create solve_me_first.go

---
+++ solve_me_first.go
@@ -0,0 +1,14 @@
+// HackerRank
+// https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+func solveMeFirst(a uint32,b uint32) uint32{
+  // Hint: Type return (a+b) below
+  return (a+b)
+}
+
+// // Solusi Ke 2
+// func solveMeFirst(a uint32,b uint32) uint32{
+//   // Hint: Type return (a+b) below
+//   var hasil uint32 = a + b
+//   return hasil
+// }
