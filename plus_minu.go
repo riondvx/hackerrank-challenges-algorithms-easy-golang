0d74ed5b253658b0e1b413dfd8fd41b5a7e535e2
Rion <rioneffendy0001repo@gmail.com>
2024-08-12 16:17:05 +0700

    Create plus_minu.go

---
+++ plus_minu.go
@@ -0,0 +1,24 @@
+// HackerRank
+// https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
+
+func plusMinus(arr []int32) {
+    // Write your code here
+    var n_positive float64 = 0.0
+    var n_negative float64 = 0.0
+    var n_zero float64 = 0.0
+    var len_arr float64 = float64(len(arr))
+    
+    for _, number := range arr {
+        if number > 0 {
+            n_positive += 1.0
+        } else if number < 0 {
+            n_negative += 1.0
+        } else {
+            n_zero += 1.0
+        }
+    }
+    
+    fmt.Println(n_positive / len_arr)
+    fmt.Println(n_negative / len_arr)
+    fmt.Println(n_zero / len_arr)
+}
